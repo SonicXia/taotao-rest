2bfc154b004bb509a7d50dea523b4bbb90d077cf
SonicXia <sonicdxf@sina.com>
2016-06-08 17:49:39 +0800

    redis缓存；jedis整合spring mvc

--- src/main/java/com/taotao/rest/service/impl/ContentServiceImpl.java
+++ src/main/java/com/taotao/rest/service/impl/ContentServiceImpl.java
@@ -19,9 +19,11 @@ public class ContentServiceImpl implements ContentService {
 	
 	@Override
 	public List<TbContent> getContentList(long contentCid) {
+		//根据内容分类id查询内容列表
 		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
 		criteria.andCategoryIdEqualTo(contentCid);
+		//执行查询
 		List<TbContent> contentList = contentMapper.selectByExample(example);
 		return contentList;
 	}
